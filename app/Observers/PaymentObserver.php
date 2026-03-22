98d2c3353b54197dc732ba5459f2215ae83f47a8
Jean Sacren <sakiwit@gmail.com>
2026-03-22 17:23:37 -0600

    Build dispatch from PaymentObserver

--- app/Observers/PaymentObserver.php
+++ app/Observers/PaymentObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\Mail\PaymentReceived;
 use App\Models\Payment;
+use Illuminate\Support\Facades\Mail;
 
 class PaymentObserver
 {
@@ -12,5 +14,7 @@ class PaymentObserver
     public function created(Payment $payment): void
     {
         $payment->enrollment->recalculateStatus();
+
+        Mail::to($payment->enrollment->prospect->email)->send(new PaymentReceived($payment));
     }
 }
